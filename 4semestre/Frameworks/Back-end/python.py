a577caaf3d031e780e95b77b7bde7475721f5d6f
Gabriel Aquila <capu.gabriel@gmail.com>
2023-03-23 21:55:58 -0300

    aJUSTE DA AC1
    Signed-off-by: Gabriel Aquila <capu.gabriel@gmail.com>

--- 4semestre/Frameworks/Back-end/python.py
+++ 4semestre/Frameworks/Back-end/python.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app= Flask(__name__)
+
+@app.route('/Calculo') # Cria uma rota
+def main():
+    print('request:', request)
+    primeira = request.args.get('primeira')
+    segunda = request.args.get('segunda')
+    if primeira and segunda:  
+        primeira = float(primeira)
+        segunda = float(segunda)
+
+        media = (primeira + segunda) / 2
+        if media >= 7:
+            resultado = 'Aprovado'
+        elif media >= 4:
+            resultado = 'Recuperação'
+        else:
+            resultado = 'Reprovado'
+    
+    return render_template('index.html', media=media,resultado=resultado)
+if __name__ == '__main__':
+    app.run(debug=True) # Executa a aplicação
+@app.route('/', methods=['GET'])
+def main():
+    return "meu primeiro site"
+
+
 
 @app.route('/', methods=['GET'])
 def main():
