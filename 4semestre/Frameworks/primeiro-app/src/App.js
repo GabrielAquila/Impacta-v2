07bf19599c2c90f2512ed030004f7fcfe3b09382
Gabriel Aquila <capu.gabriel@gmail.com>
2023-04-20 21:36:22 -0300

    Componentes  chamando api externas
    Signed-off-by: Gabriel Aquila <capu.gabriel@gmail.com>

--- 4semestre/Frameworks/primeiro-app/src/App.js
+++ 4semestre/Frameworks/primeiro-app/src/App.js
@@ -4,6 +4,8 @@ import Cadastro from './Cadastro';
 import Usuario from './Usuario';
 import Retorna from './Retorna';
 import Json from './Json';
+import RickAndMortyCharacters from './RickAndMortyCharacters';
+import Joke from './Joke';
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
     <p><Link to='/usuario'>Usuario</Link></p>
     <p><Link to='/retorna'>Retorna</Link></p>
     <p><Link to='/json'>Json</Link></p>
+    <p><Link to='/RickAndMortyCharacters'>RickAndMortyCharacters</Link></p>
+    <p><Link to='/Joke'>Joke</Link></p>
     </header>
     <main>
         <Routes>
@@ -20,7 +24,10 @@ export default function App() {
           <Route path='/cadastro' element= {<Cadastro/>}/>
           <Route path='/retorna' element= {<Retorna/>}/>
           <Route path='/json' element= {<Json/>}/>
+          <Route path='/RickAndMortyCharacters' element= {<RickAndMortyCharacters/>}/>
+          <Route path='/Joke' element= {<Joke/>}/>
         </Routes>
       </main></>
+
   );
 }
